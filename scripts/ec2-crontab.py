89a78c93d773e0da19fe0f836307572c37772fc5
urluba <lucas.barriere@gmail.com>
2017-01-13 01:39:07 +0000

    A debugger

--- scripts/ec2-crontab.py
+++ scripts/ec2-crontab.py
@@ -2,188 +2,192 @@
 # coding: utf8
 
 from __future__ import unicode_literals
+
 import argparse
 import json
 import logging
 import boto3
-from botocore.exceptions import ProfileNotFound
+from botocore.exceptions import ProfileNotFound, ClientError
 import os
+from croniter import croniter
+from datetime import datetime
 
-'''
--------------------------------------------------------------------------------
-# Establish a session with AWS
-#
--------------------------------------------------------------------------------
-'''
+def _init_aws_session(boto_profile='dev'):
+    """Return a session to query AWS API
 
-
-def _init_aws_session(boto_profile='int'):
+    :param boto_profile: name of the session profile
+    :type boto_profile: string
+    :return: Boto3 service client instance
+    :rtype: boto3.session.Session
+    """
     # Let's go
     try:
-        session = boto3.Session(profile_name=boto_profile)
+        if boto_profile:
+            session = boto3.Session(profile_name=boto_profile)
+        else:
+            session = boto3.Session()
     except ProfileNotFound as e:
         logging.error(e)
-        print
-        e
+        print e
         raise SystemExit, 1
 
     return session
 
+def tag_to_date(value):
+    ''' Transcode the value of a auto:* tag to a date function
 
-'''
--------------------------------------------------------------------------------
-#
-# http://docs.ansible.com/ansible/developing_inventory.html
--------------------------------------------------------------------------------
-'''
-def instances_get(id=None, states=None):
-    ''' instances_get()
+    :param value: The value of the auto:* tag_to_date
+    :type value: string
+    :return: date of the event
+    :rtype: datetime
+    '''
+    if value == 'now':
+        return datetime.now()
 
-    Return the list of all instances matching filters
+    logging.error("Unable to transcode [%s] to a date!"
+        " Event set in the futur." % value)
+
+    return datetime.date(datetime.max)
+
+def ec2_apply_cron(profile_name = False, id = False, dry_run = True):
+    ''' Return the list of all instances matching filters
+
+    :param id: Run only on a specific instances
+    :type id: string
+    :return: don't know yet, nb of changed, total ?
+    :rtype: array ?
 	'''
-    logging.debug("+ Looking for all running instances")
+    logging.debug("+ Looking for all tagged instances")
 
-    session = _init_aws_session()  # Init a BOTO3 session
+    session = _init_aws_session(profile_name)  # Init a BOTO3 session
     ec2 = session.resource(u'ec2')
-    fields=["id", "tags", "key_name"]
 
-    Filters = []
+    # Retrieve EC2 instances stopped or running with handled tags
+    Filters = [
+        {
+            'Name': 'tag-key',
+            'Values': ['auto:stop', 'auto:start'],
+        },
+        {
+            'Name': 'instance-state-name',
+            'Values': ['stopped','running'],
+        }
+    ]
 
-    if id is not None:
+    if id:
         logging.debug("+ Finally, looking only for %s", id)
         Filters.append({
             'Name': 'instance-id',
             'Values': [id],
         })
 
-    if states is not None:
-        logging.debug("+ I search %s instances" % states)
-        Filters.append({
-            'Name': 'instance-state-name',
-            'Values': states,
-        })
-
-    instances = ec2.instances.filter(Filters=Filters)
-
-    ansible_inventory = {"_meta": {"hostvars": {}}}
+    instances = ec2.instances.filter(Filters = Filters)
+    instances_to_start = []
+    instances_to_stop = []
 
     for instance in instances:
-        logging.debug("+Found %s" % instance.id)
+        logging.debug("+ Found [%s]" % instance.id)
 
-        if instance.tags and instance.private_ip_address:
-            instance_vars = {}
+        instance_start_at = datetime.date(datetime.max)
+        instance_stop_at = instance_start_at
 
-            # parse each key and reformat some
-            for tag in instance.tags:
-                # deja crade... :'(
-                k = tag['Key'].decode('utf-8').lower()
-                key = {
-                    "asg": u"role", # To be removed
-                    "role": u"role",
-                    "squidport": u"squid_port",
-                    "squidwithssl": u"squid_ssl",
-                    "squidwithantivirus": u"squid_antivirus",
-                    "proxyprotocol": u'squid_proxy_protocol',
-                    "squidconfigfile": u"squid_config_file",
-                }.get( k, u"ec2_" + k )
-
-                value = tag['Value'].decode('utf-8').lower()
-
-                instance_vars[key] = value
-
-            # Add some attributes
-            for a in ['private_dns_name', 'public_dns_name', 'image_id', ]:
-                if getattr(instance, a, False):
-                        instance_vars[u'ec2_' + a] = getattr(instance, a).decode('utf-8')
-
-            # Placement is an array I don't want
-            if instance.placement:
-                instance_vars[u'ec2_availabilityzone'] = \
-                    instance.placement['AvailabilityZone'].decode('utf-8')
-                instance_vars[u"ec2_region"] = instance_vars["ec2_availabilityzone"][:-1]
-
-            # Must specify full path of key file
-            if instance.key_name:
-                instance_vars[u"ansible_ssh_private_key_file"] = \
-                    u"~/.ssh/%s.pem" % instance.key_name.decode('utf-8')
-
-            # Ansible use private_ip_address. If not there, instance can't be added
-            instance_vars[u"ec2_private_ip_address"] = instance_vars[u"ansible_host"] = \
-                instance.private_ip_address.decode('utf8')
-
-            # Default user name for ansible is ec2-user
-            instance_vars[u"ansible_user"] = {
-                "ami-64bb2317": u"gtsadmin",    # RET, eu-west-1
-                "ami-67fd0908": u"gtsadmin",    # RET, eu-central-1
-                "ami-98345feb": u"admin",       # FortiOS custom, eu-west-1
-                "ami-87e91fe8": u"admin",       # FortiOS custom, eu-central-1
-                "ami-8af48ef9": u"admin",       # BigIP, eu-west-1
-                "ami-fbeb9188": u"admin",
-                "ami-cbf58fb8": u"admin",
-                "ami-c8f58fbb": u"admin",
-                "ami-51f18b22": u"admin",
-                "ami-7bf38908": u"admin",
-                "ami-b5f68cc6": u"admin",
-                "ami-65f58f16": u"admin",
-                "ami-91f18be2": u"admin",
-                "ami-2157aa4e": u"admin",       # BigIP, eu-central-1
-                "ami-8055a8ef": u"admin",
-                "ami-2c56ab43": u"admin",
-                "ami-2f57aa40": u"admin",
-                "ami-2854a947": u"admin",
-                "ami-2657aa49": u"admin",
-                "ami-e357aa8c": u"admin",
-                "ami-2557aa4a": u"admin",
-                "ami-cb55a8a4": u"admin",
-                "ami-082a4d7b": u"admin",       # BigIP custom, eu-west-1
-            }.get( getattr( instance, "image_id", None), u"ec2-user" )
-
-
-            # Build groups named after list values
-            for g in ["role", "ec2_aws:cloudformation:stack-name"]:
-                # Instance has a label/key named as g
-                if instance_vars.get(g, False):
-
-                    # If needed Create group in inventory
-                    if not ansible_inventory.get(instance_vars[g]):
-                        logging.warning("Create group %s" % instance_vars[g])
-                        ansible_inventory[instance_vars[g]] = {'hosts': []}
-
-                    ansible_inventory[instance_vars[g]]['hosts'].append(instance.id)
-                    logging.warning("adding %s to group %s" % (instance.id, instance_vars[g]) )
-
-            # Add this instance to global inventory
-            ansible_inventory['_meta']['hostvars'].update(
-                { instance.id: instance_vars }
-            )
+        if getattr(instance, 'state', False):
+            instance_state = instance.state.get('Name')
         else:
-            logging.warning("%s has no tag or no private ip (skipped)" % instance.id)
+            logging.error("Unable to get [%s] state" % instance.id)
+            continue
 
-    # end for instance in instances
+        if instance_state != 'stopped' and instance_state != 'running':
+            logging.error("[%s] has unsupported [%s] state" %
+                (instance.id, instance_state))
+            continue
 
-    return ansible_inventory
+        for tag in instance.tags:
+            k = tag['Key'].decode('utf-8').lower()
+            if k == 'auto:start':
+                instance_start_at = tag_to_date(
+                    tag['Value'].decode('utf-8').lower()
+                )
 
+            if k == 'auto:stop':
+                instance_stop_at = tag_to_date(
+                    tag['Value'].decode('utf-8').lower()
+                )
 
-def instance_get(id=None, fields=["id", "state"]):
-    ''' instance_get( instanceId)
+        if instance_start_at == instance_stop_at:
+            logging.error("%s is [%s] and must be up from: [%s] to [%s])!" %
+                (instance.id, instance_state,
+                instance_start_at, instance_stop_at)
+            )
+            continue
 
-	Return the state of the instance specified by id
-	'''
+        logging.info("+ [%s] is [%s], wake at [%s]"
+            " and stop at [%s]" %
+            (instance.id, instance_state,
+            instance_start_at, instance_stop_at)
+        )
 
-    return instances_get(id=id, fields=fields)
+        # There is something to optimize there to avoid
+        # all these calls... Build the list first and parse after ?
+        current_date = datetime.now()
 
+        if instance_state == "running":
+            if current_date > instance_stop_at:
+                logging.debug("++ I must stop it!")
+                instances_to_stop.append(instance.id)
 
+            continue
 
+        if instance_state == "stopped":
+            if current_date > instance_start_at:
+                logging.debug("++ Wake up!")
+                instances_to_start.append(instance.id)
 
-'''
--------------------------------------------------------------------------------
--------------------------------------------------------------------------------
-'''
+            continue
+
+        logging.error("Untrapped state [%s]" % instance_state)
+        # continue
+
+    output = {}
+    '''
+    dry_run = True
+    if instances_to_start:
+        output['to_start'] = instances_to_start
+        try:
+            result = ec2.instances.filter(
+             InstanceIds = instances_to_start,
+             DryRun = dry_run,
+            ).start()
+        except ClientError as e:
+            if e.response['Error'].get('Code') == 'DryRunOperation':
+                logging.debug(e.response['Error'])
+            else:
+                logging.error(e)
+            output['start_result'] = e.response['Error'].get('Message')
+
+    if instances_to_stop:
+        output['to_stop'] = instances_to_stop
+        try:
+            result = ec2.instances.filter(
+             InstanceIds = instances_to_stop,
+             DryRun = dry_run,
+            ).stop()
+        except ClientError as e:
+            if e.response['Error'].get('Code') == 'DryRunOperation':
+                logging.debug(e.response['Error'])
+            else:
+                logging.error(e)
+            output['stop_result'] = e.response['Error'].get('Message')
+    '''
+
+    return json.dumps(output)
+
 if __name__ == '__main__':
-    # Take care of the command line
     parser = argparse.ArgumentParser(
         description = """Start or stop an instance based on the value of
-        auto:start and auto:stop tags"""
+        auto:start and auto:stop tags.
+        !!! BEWARE TZ and local time !!!
+        """
     )
 
     parser.add_argument(
@@ -195,7 +199,7 @@ if __name__ == '__main__':
         required = False,
         action = 'store',
         dest = 'profile_name',
-        default = 'int',
+        default = 'dev',
     )
 
     parser.add_argument(
@@ -215,19 +219,23 @@ if __name__ == '__main__':
         default = False,
     )
 
+    parser.add_argument('--dry-run',
+        help = 'do nothing',
+        required = False,
+        action = 'store_true',
+        dest = 'dry_run',
+        default = False,
+    )
+
     args = parser.parse_args()
 
     if args.debug_mode:
         logging.basicConfig(level=logging.DEBUG)
 
     if not args.profile_name:
-        args.PROFILENAME = os.environ.get('AWS_PROFILE', False)
+        args.profile_name = os.environ.get('AWS_PROFILE', False)
 
     if args.instance_id:
         logging.info("- Looking for: %s..." % args.instance_id)
 
-
-
-    logging.info("- Building inventory for all filtered EC2")
-    #_build_inventory(args.PROFILENAME)
-    #print json.dumps(instances_get())
+    print ec2_apply_cron(args.profile_name, args.instance_id)
